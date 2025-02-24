39120ebeb391373267b23140cb331d0b8a96ab4a
Chris Miller <3810325+ChrisGMlr@users.noreply.github.com>
2025-02-24 12:30:01 -0500

    Adding Blueprint to seperate responsiblities.

--- app.py
+++ app.py
@@ -1,74 +1,18 @@
-import os
-
 import sqlalchemy
-from sqlalchemy import create_engine, MetaData, select
-import logging
 
-from flask import Flask, jsonify, request
-from flask.cli import load_dotenv
-from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.ext.automap import automap_base
+from flask import Flask
 
-from utils import return_user
-
-load_dotenv()
-logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+from controllers.users import user_bp
 
 app = Flask(__name__)
+app.register_blueprint(user_bp, url_prefix="/users")
 
-#Data base connection and ORM mapping
-db_connection = os.getenv("db_connection")
-engine = create_engine(db_connection, echo=True)
-metadata_obj = MetaData()
 
-metadata_obj.reflect(engine, only=['users', 'portfolio'])
-
-Base = automap_base(metadata = metadata_obj)
-Base.prepare()
-schema = {
-    'type': 'object',
-    'properties': {
-        'name': {'type': 'string'},
-        'email': {'type': 'string'},
-        'password': {'type': 'string'}
-    },
-    'required': ['email', 'password']
-}
 @app.route('/')
 def index():
     app.logger.info(f"SQL Alchemy v: {sqlalchemy.__version__}")
     return 'App is healthy'
 
-
-@app.route('/users', methods=['GET'])
-def getallusers():
-    app.logger.info("Trying to get all users")
-    user = Base.classes.users
-    with engine.connect() as conn:
-        result = conn.execute(select(user))
-        user_list = return_user(result)
-
-    return jsonify(user_list)
-
-@app.route('/users/<user_id>', methods=['GET'])
-def get_single_user(user_id):
-    app.logger.info(f"Trying to get user with id: {user_id}")
-    user = Base.classes.users
-    with engine.connect() as conn:
-        result = conn.execute(select(user).where(user.user_id == user_id))
-        user_list = return_user(result)
-
-    return jsonify(user_list)
-
-@app.route('/users/', methods = ['POST'])
-#@expects_json(schema)
-def add_new_user():
-    data = request.get_json()
-    user = Base.classes.users
-    with engine.connect() as conn:
-        conn.execute(insert(user).values(data))
-        conn.commit()
-    return jsonify(isError = False, message = "User added successfully", statusCode = 200)
 if __name__ == '__main__':
     app.run(debug=True)
 
---
+++ controllers/users.py
@@ -0,0 +1,57 @@
+import os
+
+from sqlalchemy import create_engine, MetaData, select
+import logging
+
+from flask import  jsonify, request, Blueprint
+from flask.cli import load_dotenv
+from sqlalchemy.dialects.postgresql import insert
+from sqlalchemy.ext.automap import automap_base
+
+from utils import return_user
+
+load_dotenv()
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
+
+user_bp = Blueprint('user', __name__)
+# Get the logger (it will inherit settings from app.py)
+logger = logging.getLogger(__name__)
+#Data base connection and ORM mapping
+db_connection = os.getenv("db_connection")
+engine = create_engine(db_connection, echo=True)
+metadata_obj = MetaData()
+
+metadata_obj.reflect(engine, only=['users', 'portfolio'])
+
+Base = automap_base(metadata = metadata_obj)
+Base.prepare()
+
+@user_bp.get('/')
+def getallusers():
+    logger.info("Trying to get all users")
+    user = Base.classes.users
+    with engine.connect() as conn:
+        result = conn.execute(select(user))
+        user_list = return_user(result)
+
+    return jsonify(user_list)
+
+@user_bp.get('/<user_id>')
+def get_single_user(user_id):
+    logger.info(f"Trying to get user with id: {user_id}")
+    user = Base.classes.users
+    with engine.connect() as conn:
+        result = conn.execute(select(user).where(user.user_id == user_id))
+        user_list = return_user(result)
+
+    return jsonify(user_list)
+
+@user_bp.post('/')
+#@expects_json(schema)
+def add_new_user():
+    data = request.get_json()
+    user = Base.classes.users
+    with engine.connect() as conn:
+        conn.execute(insert(user).values(data))
+        conn.commit()
+    return jsonify(isError = False, message = "User added successfully", statusCode = 200)
